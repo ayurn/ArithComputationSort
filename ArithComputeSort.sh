717bd2ec03cb04f4e6959951125fed702f1eb7ec
ayurn <65171090+ayurn@users.noreply.github.com>
2021-04-08 23:01:19 +0530

    Added : Dictionary to Array

--- ArithComputeSort.sh
+++ ArithComputeSort.sh
@@ -26,3 +26,13 @@ dict1[result1]="$result1"
 dict1[result2]="$result2"
 dict1[result3]="$result3"
 dict1[result4]="$result4"
+
+#for (( i-1; i<=${dict1[@]}; i++ ))
+
+for i in "${dict1[@]}"
+do 
+	arr[$x]=$i
+	let x++
+done
+echo ${arr[@]}
+#echo ${arr[#]}
